267843774959fe4554d6f04c3219b4e9272ba4b3
bbtarzan12 <bbtarzan12@gmail.com>
2019-05-09 12:57:18 +0900

    런타임에서 Controller를 결정하기 위한 함수 작성

--- AI_Connect4/main.cpp
+++ AI_Connect4/main.cpp
@@ -2,10 +2,62 @@
 #include "CommandLineRenderer.h"
 #include "KeyboardController.h"
 #include "AIAlphaBetaController.h"
-#include <iostream>
 #include "AIMCTSController.h"
 #include "AIPureMCTSController.h"
 
+#include <iostream>
+#include <string>
+#include "AIRandomController.h"
+
+using namespace std;
+
+
+void SetPlayer(const shared_ptr<Game>& game)
+{
+	string name;
+	cout << "Player Name : ";
+	while (!(getline(cin, name)) || name.empty())
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Incorrect input. Try again : ";
+	}
+
+	int ai;
+	cout << "1. AlphaBeta" << endl;
+	cout << "2. MCTS UCT" << endl;
+	cout << "3. MCTS Pure" << endl;
+	cout << "4. Random" << endl;
+	cout << "5. Keyboard" << endl;
+	cout << "Select controller : ";
+
+	while (!(cin >> ai) || ai <= 0 || ai > 5)
+	{
+		cin.clear();
+		cin.ignore();
+		cout << "Incorrect input. Try again : ";
+	}
+
+	switch (ai)
+	{
+		case 1:
+			game->AddPlayer<AIAlphaBetaController>(name);
+			break;
+		case 2:
+			game->AddPlayer<AIMCTSController>(name);
+			break;
+		case 3:
+			game->AddPlayer<AIPureMCTSController>(name);
+			break;
+		case 4:
+			game->AddPlayer<AIRandomController>(name);
+			break;
+		case 5:
+			game->AddPlayer<KeyboardController>(name);
+			break;
+	}
+	cin.ignore();
+}
 
 int main()
 {
@@ -14,8 +66,8 @@ int main()
 
 	game->Initialize();
 
-	game->AddPlayer<AIAlphaBetaController>("Alpha Beta 11");
-	game->AddPlayer<KeyboardController>("Player");
+	SetPlayer(game);
+	SetPlayer(game);
 
 	while (!game->IsGameFinished())
 	{
